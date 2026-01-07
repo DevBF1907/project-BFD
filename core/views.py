0576ffa2fc35ed0de9e24a8c4097b5b4cb684c9b
DevBF1907 <devbrenno1907@gmail.com>
2026-01-07 00:22:52 -0300

    fix: remove comments

--- core/views.py
+++ core/views.py
@@ -58,23 +58,14 @@ class LojaSerializer(ModelSerializer):
 
 
 class UserViewSet(ModelViewSet):
-    """
-    CRUD de usuários.
-    Acesso exclusivo do ADMIN.
-    """
+   
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAdmin]
 
 
 class LojaViewSet(ModelViewSet):
-    """
-    CRUD de lojas.
 
-    ADMIN   → acesso total
-    TECNICO → leitura de lojas ativas
-    CLIENTE → apenas sua própria loja
-    """
     queryset = Loja.objects.all()
     serializer_class = LojaSerializer
 
@@ -114,13 +105,7 @@ def pode_ver_loja(user, loja_id) -> bool:
 
 
 class AparelhoViewSet(ModelViewSet):
-    """
-    CRUD de aparelhos.
-
-    ADMIN   → acesso total
-    TECNICO → acesso total
-    CLIENTE → leitura apenas da própria loja
-    """
+  
     queryset = Aparelho.objects.all()   # 🔥 ESSENCIAL
     serializer_class = AparelhoSerializer
     permission_classes = [IsAdminOrTecnico]
